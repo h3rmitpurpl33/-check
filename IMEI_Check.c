53f40fe377fb58a144bc51642310b59eca496ae1
h3rmit <76179581+h3rmitpurpl33@users.noreply.github.com>
2021-08-30 00:28:11 +0300

    Update IMEI_Check.c

--- IMEI_Check.c
+++ IMEI_Check.c
@@ -1,40 +1,32 @@
 
 
-int main(){
-char chk_digit;
-int i , ch,sum,temp;
-sum = 0;
-printf("enter IMEI (15 digits) : ");
-for(i=1;i<15;i++)
-{
-    ch = getchar();
-    if((i & 1) == 1)
-    {
-        sum += ch - '0';
-    }
-    else
-    {
-        temp = 2*(ch -'0');
-        if(temp >= 10)
-        {
-            temp = (temp/10) + (temp%10);
+int main() {
+    char chk_digit;
+    int i, ch, sum, temp;
+    sum = 0;
+    printf("enter IMEI (15 digits) : ");
+    for (i = 1; i < 15; i++) {
+        ch = getchar();
+        if ((i & 1) == 1) {
+            sum += ch - '0';
+        } else {
+            temp = 2 * (ch - '0');
+            if (temp >= 10) {
+                temp = (temp / 10) + (temp % 10);
+            }
+            sum += temp;
         }
-        sum += temp;
     }
-}
-ch = getchar();
-ch = ch - '0';
-chk_digit = sum % 10;
-if(chk_digit != 0)
-{
-    chk_digit = 10 - chk_digit;
-}
-if(ch == chk_digit)
-{
-    printf("****valid IMEI****\n");
-}else
-{
-    printf("****invalid IMEI****\n");
-}
-return 0;
+    ch = getchar();
+    ch = ch - '0';
+    chk_digit = sum % 10;
+    if (chk_digit != 0) {
+        chk_digit = 10 - chk_digit;
+    }
+    if (ch == chk_digit) {
+        printf("****valid IMEI****\n");
+    } else {
+        printf("****invalid IMEI****\n");
+    }
+    return 0;
 }
